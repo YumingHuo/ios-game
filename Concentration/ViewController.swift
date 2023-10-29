011028f100b23872c2d868d748b59f92143979a6
YumingHuo <hym811@outlook.com>
2023-10-29 15:56:26 +0000

    update emoji list

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -52,13 +52,15 @@ class ViewController: UIViewController {
     }
 
     
-    private var emojiChoices = ["🎃","👻","🍭","😈","🦇","😨","🍎","🍬","😱"]
-    
+//    private var emojiChoices = ["🎃","👻","🍭","😈","🦇","😨","🍎","🍬","😱"]
+    private var emojiChoices = "🎃👻🍭😈🦇😨🍎🍬😱"
+
     private var emoji = [Card: String] ()
 
     private func emoji(for card: Card) -> String {
         if emoji[card] == nil, emojiChoices.count > 0 {
-            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
             }
         return emoji[card] ?? "?"
     }
