5012b53ddb3db75c3f5d9284ce990050a68e818e
YumingHuo <hym811@outlook.com>
2023-10-29 16:59:41 +0000

    improve only face up card

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,17 +12,19 @@ struct Concentration {
     
     private var indexOdOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    }else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            let ch = "h".oneAndOnly
+            return cards.indices.filter {cards[$0].isFaceUp}.oneAndOnly
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    }else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -52,5 +54,11 @@ struct Concentration {
             let card = Card(identifier: identifier)
             cards += [card, card]
         }
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
